a58bbe39172e72f75e420b4e614626b25081a259
naskar156 <souravnaskar156@gmail.com>
2020-04-24 06:14:04 +0530

    commit

---
+++ process.php
@@ -0,0 +1,21 @@
+<?php
+if(isset($_POST['submit'])){
+/*$to = "souravnaskar156@gmail.com";
+$email=$_POST['email'];
+$subject = "My subject";
+$txt = "Hello world!";
+$headers = "From: naskar.sourav818@gmail.com" . "\r\n" .
+"CC: somebodyelse@example.com";
+
+mail($to,$subject,$txt,$headers);
+header("Location :contact.html");
+}*/
+echo "It works"
+?>
+
+
+
+
+
+
+
